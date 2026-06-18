befa5ee0798d8d74f03246fb06a999f93138d12e
Megh Shetty <meghvshetty@outlook.com>
2026-06-18 12:35:03 +0100

    added health check to proxy server

--- herald.go
+++ herald.go
@@ -3,6 +3,7 @@ package main
 import (
 	"github.com/c0d3x-io/herald/internal/config"
 	"log/slog"
+	"net/http"
 	"os"
 )
 
@@ -22,4 +23,14 @@ func main() {
 		"ListenerAddr", cfg.ListenAddr,
 		"UpstreamURL", cfg.UpstreamURL,
 	)
+
+	// Proxy sever to ensure it's working and online
+	server := http.NewServeMux()
+	server.HandleFunc("GET /health", func(w http.ResponseWriter, r *http.Request) {
+		w.WriteHeader(http.StatusOK)
+		w.Write([]byte("Herald:ok"))
+	})
+
+	http.ListenAndServe(":8080", server)
+
 }
