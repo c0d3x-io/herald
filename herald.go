6a62fbac4b6c1e74b4c238318a645fb0be295b86
Megh Shetty <meghvshetty@outlook.com>
2026-06-18 11:09:22 +0100

    added log using log/slog package

--- herald.go
+++ herald.go
@@ -1,18 +1,25 @@
 package main
 
 import (
-	"fmt"
 	"github.com/c0d3x-io/herald/internal/config"
+	"log/slog"
 	"os"
 )
 
 func main() {
+	// Load log ---> YAGNI — You Aren't Gonna Need It.
+	logger := slog.New(slog.NewJSONHandler(os.Stdout, &slog.HandlerOptions{
+		Level: slog.LevelInfo,
+	}))
+
+	// Load config
 	cfg, err := config.LoadConfig()
 	if err != nil {
-		fmt.Println("config error", "error", err)
+		logger.Error("config error", "error", err)
 		os.Exit(1)
 	}
-	fmt.Println("---Herald Started--")
-	fmt.Printf("ListerAddr: %s", cfg.ListenAddr)
-	fmt.Printf("Upstream: %s", cfg.UpstreamURL)
+	logger.Info("---Herald Started--",
+		"ListenerAddr", cfg.ListenAddr,
+		"UpstreamURL", cfg.UpstreamURL,
+	)
 }
